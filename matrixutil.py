4f0f6503e080a27f260c8260e58687e2f6887dcb
Alberto Paoluzzi <apaoluzzi@mac.com>
2013-01-30 10:47:38 +0100

    introduced matrixutil.py

---
+++ matrixutil.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+The MIT License
+===============
+    
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
+"""
+
+def csrProduct(CSRm1,CSRm2):
+    CSRm = CSRm1 * CSRm2
+    return CSRm
+
+"""
+if __name__ == "__main__" and self_test:
+    print "\n>>> csrProduct"
+    CSRm = csrProduct(csrFV, csrTranspose(csrEV))
+    print "\ncsrFE =\n", csrToMatrixRepresentation(CSRm)
+"""
