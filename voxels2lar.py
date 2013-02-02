e7498ada990d222f71892bf5b96f91424ba70acc
Alberto Paoluzzi <apaoluzzi@mac.com>
2013-02-02 20:14:40 +0100

    introduced bigger 2D lar generation example

--- voxels2lar.py
+++ voxels2lar.py
@@ -6,23 +6,15 @@ from lar import *
 
 def larFromImageBlocks(blocks):
     dim = len(blocks[0][0])
-    print "\ndim =\n",dim
     blocks = AA(CAT)(blocks)
-    print "\nblocks =\n",blocks
     bounds = TRANS(blocks)
-    print "\nbounds =\n",bounds
     cells = [[] for k in range(len(blocks))]
-    print "\ncells =\n",cells
     minMaxCoords = zip(AA(min)(bounds[:dim]), AA(max)(bounds[dim:]))
-    print "\nminMaxCoords =\n",minMaxCoords
     gridPoints = CART(AA(FROMTO)(AA(list)(minMaxCoords)))
-    print "\ngridPoints =\n",gridPoints
     counter,V,i = [],[],0
     for point in gridPoints:
         for k,cell in enumerate(blocks):
-            print k,cell
             pmin,pmax = cell[:dim],cell[dim:]
-            print "\npmin,pmax =", (pmin,pmax)
             classify = AND(CAT([AA(ISGE)(TRANS([pmin,point])),AA(ISLE)(TRANS([pmax,point]))]))
             if classify: counter.append(k)
         if len(counter) >= dim+1:
@@ -40,11 +32,8 @@ if __name__=="__main__":
     
     model = larFromImageBlocks(blocks)
     V,cells = model
-    print "\nV =\n",V
-    print "\ncells =\n",cells
     V,faces = larSkeletons(model,dim=2)
     F0V, F1V, F2V = faces
-    print "AA(LEN)([F0V, F1V, F2V]) =", AA(LEN)([F0V, F1V, F2V])
     V = model[0]
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F2V[:-4])) ))
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F1V)) ))
@@ -58,14 +47,31 @@ if __name__=="__main__":
     
     model = larFromImageBlocks(blocks)
     V,cells = model
-    print "\nV =\n",V
-    print "\ncells =\n",cells
     V,faces = larSkeletons(model,dim=3)
     F0V, F1V, F2V, F3V = faces
-    print "AA(LEN)([F0V, F1V, F2V, F3V]) =", AA(LEN)([F0V, F1V, F2V, F3V])
     V = model[0]
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F3V[:-6])) ))
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F2V)) ))
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F1V)) ))
     VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F0V+F1V+F2V+F3V[:-6])) ))
+
+
+#-------------------------------------------------------------------
+# bigger 2D image example
+
+rects = [
+[283,388,12,7],[288,395,10,16],[291,411,26,8],[309,401,12,10],[317,411,4,3],[298,405,3,6],[301,409,8,2],[296,419,10,3],[306,419,5,2],[286,395,2,6],[295,391,2,4],[298,399,2,6],[285,386,7,2],[284,395,2,2],[289,411,2,4],[317,414,3,2],[305,408,4,1],[321,402,1,8],[287,401,1,5],[298,396,1,3],[297,393,1,2],[282,391,1,2],[292,387,2,1],[294,419,2,2],[311,419,4,1],[317,416,2,1],[284,387,1,1],[288,385,1,1],[285,397,1,1],[308,407,1,1],[293,419,1,1],[317,417,1,1],[319,399,1,1],[315,397,1,1],[311,399,1,1],[310,400,2,1],[319,400,2,1],[312,398,7,3],
+[299,381,27,16],[276,422,50,9],[276,398,10,24],[301,397,8,10],[322,397,4,25],[276,381,6,17],[282,381,17,4],[295,385,4,5],[286,415,4,7],[317,418,5,4],[311,420,6,2],[301,407,4,2],[286,411,3,4],[286,406,2,5],[290,419,3,3],[282,395,2,3],[282,385,2,3],[292,385,3,2],[297,390,2,3],[309,397,3,2],[319,397,3,2],[319,416,3,2],[320,414,2,2],[305,407,3,1],[299,397,2,2],[300,399,1,6],[298,393,1,3],[284,385,4,1],[306,421,5,1],[289,385,3,1],[312,397,3,1],[316,397,3,1],[282,388,1,3],[309,399,1,2],[321,399,1,3],[321,410,1,4],[293,421,3,1],[286,401,1,5],[282,393,1,2],[290,415,1,4],[284,386,1,1],[294,387,1,1],[295,390,2,1],[284,397,1,1],[293,420,1,1],[318,417,1,1],[315,419,2,1],[320,399,1,1],[310,399,1,1],
+ ]
+
+blocks = [[[rect[0],rect[1]],[rect[0]+rect[2], rect[1]+rect[3]]] for rect in rects] + [ [[276,381],[326,381]], [[276,381],[276,431]], [[276,431],[326,431]], [[326,326],[326,431]] ]
+
+model = larFromImageBlocks(blocks)
+V,cells = model
+V,faces = larSkeletons(model,dim=2)
+F0V, F1V, F2V = faces
+V = model[0]
+VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F2V[:-4])) ))
+VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F1V)) ))
+VIEW(EXPLODE(1.2,1.2,1.2)( MKPOLS((V,F0V+F1V+F2V[:-4])) ))
 
